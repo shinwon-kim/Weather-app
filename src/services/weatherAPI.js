17c1691f9202c5eb30a3a775ee994b50b9f2ba29
shinwonkim <shinwonkkim@gmail.com>
2025-08-06 23:48:47 +0900

    fix: api URL을 https로 변경하여 mixed content 오류 해결

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const fetchCurrentWeather = async(city) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
     const response = await axios.get(url);
     return response.data;
 }
 
 
 export const fetchForcastWeather = async(city) => {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`;
     const response = await axios.get(url);
     return response.data;
 }
