aa1ef81ab53141ee9c3424eeaa9acc9f6102c361
shinwonkim <shinwonkkim@gmail.com>
2025-08-08 20:27:00 +0900

    feat: 날씨 예보 기온 추가

--- src/components/ForecastWeather.js
+++ src/components/ForecastWeather.js
@@ -8,10 +8,12 @@ function ForecastWeather({forecast}){
           {preview10.map((item, index) => {
             const weatherIcon = item.weather[0].icon;
             const date = item.dt_txt.slice(10,-3);
+            const weatherTemp = Math.round(item.main.temp-273.15);
             return(
                 <div key={index} className="forecast-item"> 
                     <p>{date}</p>
                     <img src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt={""}></img>
+                    <p>{weatherTemp}°C</p>
                 </div>
 
             )
